0d6bcc5d10b59b43f1bb578b66338d5109ac2430
github-main-user <kolya.skripn@mail.ru>
2025-02-25 12:04:43 +0300

    add json to category list converter

--- src/models.py
+++ src/models.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name
@@ -17,3 +20,26 @@ class Category:
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+def convert_json_to_categories(json_path: str) -> list[Category]:
+    """Конвертирует список категорий в JSON формате в список объектов класса Category."""
+    with open(json_path, 'r', encoding="utf-8") as file_obj:
+        categories = json.load(file_obj)
+
+    return [
+        Category(
+            name=category['name'],
+            description=category['description'],
+            products=[
+                Product(
+                    name=product['name'],
+                    description=product['description'],
+                    price=product['price'],
+                    quantity=product['quantity'],
+                )
+                for product in category.get('products', [])
+            ],
+        )
+        for category in categories
+    ]
