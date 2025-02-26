22378d1055b1b6c3a718d161abf2a4309b53d390
github-main-user <kolya.skripn@mail.ru>
2025-02-26 15:19:31 +0300

    update category class

--- src/models.py
+++ src/models.py
@@ -16,10 +16,19 @@ class Category:
     def __init__(self, name: str, description: str, products: list[Product]) -> None:
         self.name = name
         self.description = description
-        self.products = products
+        self._products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product) -> None:
+        """Добавляет объект класса Product к списку продуктов."""
+        self._products.append(product)
+
+    @property
+    def products(self) -> list[str]:
+        """Выводит список товаров ввиде строк формата "<Название продукта>, <80 руб>. <Остаток: 15 шт>" ."""
+        return [f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.' for product in self._products]
 
 
 def convert_json_to_categories(json_path: str) -> list[Category]:
